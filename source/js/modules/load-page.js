5e5d6511fbd54795b44f0d8259c88cead3b46bde
Kapitonov Roman <kapitroman@yandex.ru>
2023-02-09 13:14:46 +0300

    анимирует фон при переключении экранов

--- source/js/modules/load-page.js
+++ source/js/modules/load-page.js
@@ -1,4 +1,12 @@
+const sectionHasTransitionEnd = ['story'];
+
 export default ()  => {
+  const screenElements = document.querySelectorAll(`.screen:not(.screen--result)`);
+  screenElements.forEach((screen) => {
+    if (sectionHasTransitionEnd.includes(screen.id)) {
+      screen.setAttribute('data-transition-end', screen.id);
+    }
+  });
   window.addEventListener('load', () => {
     document.body.classList.add('loaded');
   });
